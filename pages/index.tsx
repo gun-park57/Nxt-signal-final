2b9e8ad0092442eaaa1db3cfa35aa3c939f4b1a9
gun-park57 <gunsjobs@gmail.com>
2025-05-07 02:31:12 +0900

    index.tsx 업데이트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '20%' }}>
-      <h1>Welcome to Nxt Signal App</h1>
-      <p>Deployment successful via Vercel!</p>
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>NXT Signal 앱에 오신 것을 환영합니다!</h1>
+      <p>이 앱은 모바일 웹에서 작동합니다.</p>
     </div>
   );
 }
