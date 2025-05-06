0d9fadf5f0406c4bc88b8644dbaa05dcf2c1f5d6
gun-park57 <gunsjobs@gmail.com>
2025-05-07 00:11:06 +0900

    pages/index.tsx 만들기

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>Welcome to Nxt Signal App</h1>
+      <p>Deployment successful via Vercel!</p>
+    </div>
+  );
+}
